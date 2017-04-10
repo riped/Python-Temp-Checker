d09ee9e380dfdab7d1262397a3a18a5581b9d11e
Sean Corrigan <seanpaulcorrigan@gmail.com>
2017-04-10 09:48:21 -0400

    Upload

--- default.py
+++ default.py
@@ -7,13 +7,14 @@
 # import libraries
 import os  # Needed for clearing the terminal in a clear manner
 import time # Needed to sleep the system
+from pprint import pprint # Needed to print the temps in a nice way
 #########
 os.system("clear")
 #########
 def intro():
     print("Hey, I dont know who you are so imma call u 🅱️ ETER")
     UserName = input("Please enter your name 🅱️ ETER: \n")
-    os.system("clear")
+    os.system("clear")  
     if UserName == "sean":
         return 
     else:
@@ -31,34 +32,40 @@ def askforcity():
         input("PRESS ENTER")
         askforcity()
     if numberofcities <= 1:
-        input("You can't compare less then 2 citys silly (PRESS ENTER)")
+        input("You can't compare less then 2 cities silly (PRESS ENTER)")
         askforcity()
     numberoftries = 1
-
-    
     my_array = []
     while numberoftries <= numberofcities:
         os.system("clear")
         print("Please enter city name", numberoftries)
         city = input("")
-        print("Please enter the temprature for city", numberoftries)
+        print("Please enter the temp for city", numberoftries)
         citytemp = input("")       
         try:  # Try to convert to a int. if not restart
           citytemp = int(citytemp)  # Define as interger
         except ValueError:  # If it isn't a whole number then
             input("PRESS ENTER TO RESTART")
             askforcity()
-        
         my_array.append((citytemp, city))
         numberoftries = numberoftries ++ 1  # Add one to the end of the tries
-        
     os.system("clear")
     min_temp, min_city = min(my_array)
     print(min_city.title(), "has lowest temperature:", min_temp)
+    print("")
+    print("All Temperature Readings")
+    print(*my_array, sep='\n')
     input("PRESS ENTER TO RESTART")
-    askforcity()
-        
-    
+    askforcity()  
 askforcity()    
 
-    
+
+
+
+
+
+
+
+
+
+
